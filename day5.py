ade0e27e8d8a66ca1d0d73ab9730dc673d94da26
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2020-01-08 20:49:59 +0000

    added check_password

--- day5.py
+++ day5.py
@@ -1,9 +1,17 @@
 def day5_main():
   # if you use ctrl + lbm, so you use multiple cursors
-  print('for example it\'s')
-  print('for example it\'s')
-  print('for example it\'s')
+  # print('for example it\'s')
+  # print('for example it\'s')
+  # print('for example it\'s')
   # ctrl + / fast comment
   # if you write program for yourself, use comments as pseuso-code
   check_password()
 
+
+def check_password():
+  username = input('What\'s your name? ')
+  password = input('What\'s your password? ')
+  password_lenght = len(password)
+  password_stars = '*' * password_lenght
+  s = f'{username}, your password {password_stars} is {password_lenght} letters long'
+  print(s)
