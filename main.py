f00c76b3474b8aa9ad771dee9363beb3fa0091f6
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2020-01-11 23:22:58 +0000

    Used extend and sorted

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from day4 import day4_main
 from hack import hack
 from day5 import day5_main
 from day6 import day6_main
+from day7 import day7_main
+
 
 
 # day2_main()
@@ -11,4 +13,5 @@ from day6 import day6_main
 # day4_main()
 # hack()
 # day5_main()
-day6_main()
+# day6_main()
+day7_main()
