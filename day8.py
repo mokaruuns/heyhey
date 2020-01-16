c82b05ab16164a04241a3c4b5c82d78dd40cf6ea
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2020-01-16 21:13:00 +0000

    Realize user_profile

--- day8.py
+++ day8.py
@@ -20,3 +20,29 @@ def day8_main():
   thisdict.setdefault('key2', 123)
   thisdict.update({'key1':'1'})
   print(thisdict)
+
+  dictionary()
+
+def dictionary():
+  #1 Create a user profile for your new game. This user profile will be stored in a dictionary with keys: 'age', 'username', 'weapons', 'is_active' and 'clan'
+
+  #2 iterate and print all the keys in the above user.
+
+  #3 Add a new weapon to your user
+
+  #4 Add a new key to include 'is_banned'. Set it to false
+
+  #5 Ban the user by setting the previous key to True
+
+  #6 create a new user2 my copying the previous user and update the age value and username value. 
+
+  user_profile = dict.fromkeys(('age', 'username', 'weapons', 'is_active', 'clan'))
+  print(*user_profile.keys())
+  user_profile.update({'weapons': 'gun'})
+  user_profile.update({'is_banned': False})
+  user_profile['is_banned'] = True 
+  user2_profile = user_profile.copy()    
+  user2_profile.update({'age': 11})
+  user2_profile.update({'username': 'kjsohs'})
+  print(user_profile)
+  print(user2_profile)
