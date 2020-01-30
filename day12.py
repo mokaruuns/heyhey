5b8ed3b9cf61e01f45568a6a2a85ddcbf7ab7de3
Marsel Akhmetov <marsel.ahmetov974@gmail.com>
2020-01-30 19:12:04 +0000

    Used docstrings

--- day12.py
+++ day12.py
@@ -2,7 +2,15 @@ def first_def(a,b):
   def second_def(a,b):
     return a+b
   return second_def
+
+def show_docstrings(info):
+  '''
+  This function prints ans tests 'info'
+  '''
+  print(info)
 def day12_main():
   print("hello, day12")
   k = first_def(1,2)
   print(k(1,3))
+  show_docstrings('hellokkk')
+  print(show_docstrings.__doc__)
